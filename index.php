e306e08a5605309a6f1a38a33f6c1fd0ba0ea42e
lstables <lstables1@gmail.com>
2016-02-18 11:41:03 +0000

    update

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    
+</head>
+<body>
+
+<div class="container">
+    <h5>API Test</h5>
+
+</div>
+
+<script type="text/javascript">
+window._tracker = window._tracker || [];
+_tracker.push(['set','API','tracking']);
+_tracker.push(['set','ClientID','ke4egk3a']);
+_tracker.push(['set','CampaignID', '000001']);
+_tracker.push(['set', 'ConfirmationUrl', 'https://www.infinety.co.uk/thanks.html']);
+_tracker.push(['send']);
+</script>
+<script src="https://api.moberce.co/js/tracker.js"></script>
+<script src="https://api.moberce.co/js/bioep.min.js"></script>
+
+</body>
+</html>
