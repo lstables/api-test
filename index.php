da3cd97225c5cd2cbef0d7d32f3e6a92b5e43d3d
lstables <lstables1@gmail.com>
2016-02-27 11:27:31 +0000

    send.php

--- index.php
+++ index.php
@@ -48,15 +48,6 @@
     </div>
 </div>
 
-<script type="text/javascript">
-window._tracker = window._tracker || [];
-_tracker.push(['set','API','tracking']);
-_tracker.push(['set','ClientID','ke4egk3a']);
-_tracker.push(['set','CampaignID', '000001']);
-_tracker.push(['set', 'ConfirmationUrl', 'https://www.infinety.co.uk/thanks.html']);
-_tracker.push(['send']);
-</script>
-<script src="https://api.moberce.co/js/tracker.js"></script>
 
 </body>
 </html>
--- send.php
+++ send.php
@@ -6,3 +6,14 @@ $message = $_POST['message'];
 
 
 echo "Message sent";
+
+
+echo "<script type=\"text/javascript\">
+window._tracker = window._tracker || [];
+_tracker.push(['set','API','tracking']);
+_tracker.push(['set','ClientID','ke4egk3a']);
+_tracker.push(['set','CampaignID', '000001']);
+_tracker.push(['set', 'ConfirmationUrl', 'https://www.infinety.co.uk/thanks.html']);
+_tracker.push(['send']);
+</script>
+<script src=\"https://api.moberce.co/js/tracker.js\"></script>";
